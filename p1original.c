5ba18d13845789343c8061f61fb584d3d78827a7
Madhuri122002 <94678055+Madhuri122002@users.noreply.github.com>
2022-01-31 05:47:20 +0000

    practiced

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("the sum of %d and %d is %d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  printf("enter any two numbers\n");
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
